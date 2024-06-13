cf388a2eafa8f1fa0e69c6ab8362c9a4d9bb15d2
combi2k2 <cumbee2k2@gmail.com>
2024-06-13 18:25:12 +0200

    feat: implement optimal transport

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include "include/class_vector.h"
 #include "include/class_polygon.h"
+#include "include/optimal_transport.h"
 #include "include/voronoi.h"
 #include "include/utils.h"
 
+#include <numeric>
 #include <iostream>
 
 int main() {
     const int n_points = 2000;
+
     std::vector<Vector> points;
-    std::vector<double> weights;
-    std::vector<double> balance_weights;
+    std::vector<double> lambdas;
+
+    double weights[n_points];
+    double balance_weights[n_points];
 
     for (int i = 0; i < n_points; i++) {
         double x = urand();
@@ -17,9 +22,18 @@ int main() {
         double w = urand();
 
         points.push_back(Vector(x, y));
-        weights.push_back(w / 4);
-        balance_weights.push_back(1.0);
+        lambdas.push_back(exp(-(points.back() - Vector(0.5, 0.5)).norm2() / 0.02));
+        weights[i] = w / 2;
+        balance_weights[i] = 1.0;
     }
+    double sum_lambda = std::accumulate(lambdas.begin(), lambdas.end(), 0.0);
+
+    for (double &lambda : lambdas)
+        lambda /= sum_lambda;
+
+    OptimalTransport solver(points, lambdas);
+    solver.solve(weights);
+
     std::vector<Polygon> Voronoi1;
     std::vector<Polygon> Voronoi2;
 
